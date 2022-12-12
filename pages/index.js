273c9bb83e5a929367d93ba72d8f0d0f8e45d1a2
omotomiwa <42933279+omotomiwa@users.noreply.github.com>
2022-12-12 13:05:52 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,266 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+import "bootstrap/dist/css/bootstrap.css";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import styled from "styled-components";
+import axios from "axios";
+
+export default function Home() {
+  const Wrapper = styled.div`
+    .contain {
+      width: 100%;
+      height: 140px;
+      background: #292929;
+      padding-left: 50px;
+      padding-top: 45px;
+    }
+
+    h1 {
+      color: #ffffff;
+      font-size: 29px;
+    }
+    h2 {
+      color: #ffffff;
+      font-size: 50px;
+      font-weight: 700;
+      line-height: 73.74px;
+    }
+    p {
+      font-weight: 400;
+      font-size: 24px;
+      line-height: 31.25px;
+      color: #000000;
+    }
+    .logo-container {
+      background: transparent;
+      border: 1px solid #ffffff;
+      width: 180px;
+      height: 60px;
+      text-align: center;
+      padding-top: 12px;
+    }
+    /* Media Query for Mobile Devices */
+    @media (max-width: 480px) {
+      h2 {
+        text-align: center;
+      }
+      .logo-container {
+        background: transparent;
+        border: 1px solid #ffffff;
+        width: 180px;
+        height: 60px;
+        text-align: center;
+        padding-top: 12px;
+        margin: 0 auto;
+        position: relative;
+        right: 24px;
+      }
+    }
+
+    //media querry for Tabs
+    @media (min-width: 768px) and (max-width: 1024px) {
+      h2 {
+        text-align: center;
+      }
+      .logo-container {
+        background: transparent;
+        border: 1px solid #ffffff;
+        width: 180px;
+        height: 60px;
+        text-align: center;
+        padding-top: 12px;
+        margin: 0 auto;
+        position: relative;
+        right: 24px;
+      }
+    }
+    .image-section {
+      width: 100%;
+      height: 450px;
+      background-size: cover;
+      background-repeat: no-repeat;
+      background-position: center center;
+      background-image: url("https://res.cloudinary.com/dl7eouhxf/image/upload/v1670762748/Rectangle_5.svg");
+      padding-left: 47px;
+      padding-top: 80px;
+    }
+
+    .main {
+      padding-left: 47px;
+      padding-right: 47px;
+      padding-top: 47px;
+    }
+
+    .search {
+      font-weight: 400;
+      font-size: 24px;
+      color: #000000;
+      line-height: 31.25px;
+    }
+    .form-control {
+      height: 44px !important;
+    }
+    .grid-container {
+      padding-top: 20px;
+      padding-bottom: 20px;
+    }
+    .images {
+      background-size: 150px;
+      width: 300px;
+      height: 300px;
+
+      border-radius: 5px;
+      background-size: cover;
+      background-repeat: no-repeat;
+      background-position: center center;
+    }
+  `;
+
+  //Logic goes here
+
+  const products = [
+    {
+      id: 1,
+      name: "Guardians of the Galaxy",
+      category: "Action",
+      image:
+        "https://res.cloudinary.com/dl7eouhxf/image/upload/v1670838147/movie-poster/guardian_galaxy.jpg",
+    },
+    {
+      id: 2,
+      name: "Avengers End Game",
+      category: "Action",
+      image:
+        "https://res.cloudinary.com/dl7eouhxf/image/upload/v1670838388/movie-poster/avengers.jpg",
+    },
+    {
+      id: 3,
+      name: "John Wick",
+      category: "Action",
+      image:
+        "https://res.cloudinary.com/dl7eouhxf/image/upload/v1670838476/movie-poster/John_wick.jpg",
+    },
+    {
+      id: 4,
+      name: "Spectre",
+      category: "Action",
+      image:
+        "https://res.cloudinary.com/dl7eouhxf/image/upload/v1670838577/movie-poster/spectre.jpg",
+    },
+    {
+      id: 5,
+      name: "Fast and Furious 8",
+      category: "Action",
+      image:
+        "https://res.cloudinary.com/dl7eouhxf/image/upload/v1670838688/movie-poster/fast_and_furious.jpg",
+    },
+    {
+      id: 6,
+      name: "Ant Man",
+      category: "Action",
+      image:
+        "https://res.cloudinary.com/dl7eouhxf/image/upload/v1670838829/movie-poster/ant_man.jpg",
+    },
+    {
+      id: 7,
+      name: "Venom",
+      category: "Action",
+      image:
+        "https://res.cloudinary.com/dl7eouhxf/image/upload/v1670838933/movie-poster/venom.jpg",
+    },
+    {
+      id: 8,
+      name: "Deadpool",
+      category: "Action",
+      image:
+        "https://res.cloudinary.com/dl7eouhxf/image/upload/v1670839070/movie-poster/deadpool.jpg",
+    },
+    {
+      id: 9,
+      name: "Black Adam",
+      category: "Action",
+      image:
+        "https://res.cloudinary.com/dl7eouhxf/image/upload/v1670839189/movie-poster/black-adam-poster.jpg",
+    },
+    {
+      id: 10,
+      name: "Wakanda",
+      category: "Action",
+      image:
+        "https://res.cloudinary.com/dl7eouhxf/image/upload/v1670839345/movie-poster/wakanda.jpg",
+    },
+    {
+      id: 11,
+      name: "Free Guy",
+      category: "Action",
+      image:
+        "https://res.cloudinary.com/dl7eouhxf/image/upload/v1670839477/movie-poster/free_guy.jpg",
+    },
+  ];
+
+  const [post, setPost] = React.useState([]);
+
+  const listing = () => {
+    axios
+      .get("https://www.omdbapi.com/?i=tt3896198&apikey=695093b9")
+      .then((response) => {
+        const myData = response.data.Ratings;
+        setPost(myData);
+        //return response.data = array;
+      });
+  };
+  React.useEffect(() => {
+    listing();
+  }, []);
+
+  return (
+    <Wrapper>
+      <div className="contain">
+        <div className="logo-container">
+          <h1>MyTestApp</h1>
+        </div>
+      </div>
+      <div className="header">
+        <div className="image-section">
+          <h2>Watch</h2>
+          <h2>something</h2>
+          <h2>incredible.</h2>
+        </div>
+      </div>
+      <div className="main">
+        <div className="search-wrapper">
+          <p className="search">Search</p>
+          <input
+            type="text"
+            className="form-control h-75"
+            placeholder="Search for movies..."
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+          />
+        </div>
+        <div className="grid-container">
+          <div className="row gx-5 gy-5">
+            {products.map((item) => (
+              <div
+                className="movies col-xs-12 col-lg-3 col-md-6"
+                key={item.name}
+              >
+                <p>{item.name}</p>
+
+                <img
+                  className=" images"
+                  src={item.image}
+                  alt="https://res.cloudinary.com/dl7eouhxf/image/upload/v1670832088/image-grid.svg"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
+}
